04a28ff133f0863b70a324460f69fc7226da82b7
DESKTOP-400O5FN\Geomatica-01 <delacruzmari1411@gmail.com>
2025-05-14 17:00:44 -0500

    cambios en el form order

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -12,6 +12,16 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Wizard\Step;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\KeyValue;
+
 
 class OrderResource extends Resource
 {
@@ -23,65 +33,103 @@ class OrderResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\Select::make('client_id')
+                Select::make('client_id')
                 ->relationship('client', 'nombre')
                 ->searchable()
                 ->preload()
                 ->createOptionForm([
-                    Forms\Components\TextInput::make('nombre')
+                    TextInput::make('nombre')
                         ->required()
                         ->maxLength(255),
-                    Forms\Components\TextInput::make('direccion')
+                    TextInput::make('direccion')
                         ->label('Direccion')
                         ->required()
                         ->maxLength(255),
-                    Forms\Components\TextInput::make('telefono')
+                    TextInput::make('telefono')
                         ->label('Telefono')
                         ->tel()
                         ->required(),
                 ])
                 ->required(),
-                Forms\Components\Select::make('project_id')
-                ->relationship('project', 'nombre')
-                ->searchable()
-                ->preload()
-                ->createOptionForm([
-                    Forms\Components\TextInput::make('nombre')
-                        ->required()
-                        ->maxLength(255),
-                    Forms\Components\TextInput::make('direccion')
-                        ->label('Direccion')
-                        ->required()
-                        ->maxLength(255),
-                    Forms\Components\TextInput::make('telefono')
-                        ->label('Telefono')
-                        ->tel()
-                        ->required(),
+                //Se agrega el pedido 
+            Repeater::make('agregar pedido')
+            ->schema([
+                Select::make('project_id')
+                    ->relationship('project', 'nombre')
+                    ->searchable()
+                    ->preload()
+                    ->createOptionForm([
+                        TextInput::make('nombre')
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('direccion')
+                            ->label('Direccion')
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('telefono')
+                            ->label('Telefono')
+                            ->tel()
+                            ->required(),
+                        ])
+                    ->required(),
+                TextInput::make('precio')
+                    ->default('0')
+                    ->maxLength(255),
+                TextInput::make('anticipo')
+                    ->default('0')
+                    ->maxLength(255),
+                TextInput::make('adeudo')
+                    ->default('0')
+                    ->maxLength(255),
+                /* ColorPicker::make('color') */
+                    ])
+                /* KeyValue::make('meta')
+                  ->keyLabel('Property name') */
+                ->columns(4)
+                ->columnSpan(2),
+                //Se muestra la suma de los productos
+                Section::make('SUMA TOTAL DE PRODUCTOS')
+                    ->description('Prevent abuse by limiting the number of requests per period')
+                    ->schema([
+                                // ...
                 ])
-                ->required(),
-                Forms\Components\TextInput::make('anticipo')
-                ->required()
-                ->maxLength(255),
-                Forms\Components\TextInput::make('adeudo')
-                ->required()
-                ->maxLength(255),
-                Forms\Components\TextInput::make('status')
-                ->maxLength(255),
-            ]);
+
+                ]);
+                
+            
+                
+                    
+
+            
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                //
+                    Tables\Columns\TextColumn::make('client.nombre')
+                    ->searchable(),
+                   /*  Tables\Columns\TextColumn::make('project.nombre')
+                    ->searchable(), */
+                    Tables\Columns\TextColumn::make('anticipo'),
+                    Tables\Columns\TextColumn::make('adeudo'),
+                    /* Tables\Columns\TextColumn::make('estatus'), */
+                    
+                    Tables\Columns\TextInputColumn::make('estatus')
+                        ->rules(['required', 'max:255'])
             ])
             ->filters([
-                //
+                Tables\Filters\SelectFilter::make('type')
+                    ->options([
+                        'cat' => 'Cat',
+                        'dog' => 'Dog',
+                        'rabbit' => 'Rabbit',
+                    ]),
             ])
             ->actions([
                 /* Tables\Actions\EditAction::make(), */
-
+               
+                
                 
             ])
             ->bulkActions([
